5cc3d202ed31cd6bbb6735bb2eb88bcfa1df9feb
狮子是lion <beiyongmail@foxmail.com>
2019-12-28 21:29:00 +0800

    add ad8400

---
+++ inc/adi_ad8400.h
@@ -0,0 +1,18 @@
+#ifndef PKG_ADI_AD8400_H__
+#define PKG_ADI_AD8400_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef PKG_USING_AD8400
+
+void ad8400_write(uint8_t addr, uint8_t value);
+
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/adi_ad8400.c
@@ -0,0 +1,88 @@
+#include <rtthread.h>
+
+
+#ifdef PKG_USING_AD8400
+
+
+#define AD8400_CS_PIN 				GET_PIN(G, 7)
+#define AD8400_SCK_PIN				GET_PIN(G, 6)
+#define AD8400_SI_PIN				GET_PIN(G, 5)
+
+#ifdef BSP_AD8400_CS_PIN
+#define AD8400_CS_PIN 				BSP_AD8400_CS_PIN
+#else
+#error No define BSP_AD8400_CS_PIN 
+#endif
+
+#ifdef BSP_AD8400_SCK_PIN
+#define AD8400_SCK_PIN				BSP_AD8400_SCK_PIN
+#else 
+#error No define BSP_AD8400_SCK_PIN 	
+#endif
+
+#ifdef BSP_AD8400_SI_PIN
+#define AD8400_SI_PIN				BSP_AD8400_SI_PIN
+#else 
+#error No define BSP_AD8400_SI_PIN 	
+#endif
+
+#define MCP_CS_SET()				rt_pin_write(AD8400_CS_PIN, PIN_HIGH)
+#define MCP_CS_CLC()				rt_pin_write(AD8400_CS_PIN, PIN_LOW)
+
+#define MCP_SCK_SET()				rt_pin_write(AD8400_SCK_PIN, PIN_HIGH)
+#define MCP_SCK_CLC()				rt_pin_write(AD8400_SCK_PIN, PIN_LOW)
+
+#define MCP_SI_SET()				rt_pin_write(AD8400_SI_PIN, PIN_HIGH)
+#define MCP_SI_CLC()				rt_pin_write(AD8400_SI_PIN, PIN_LOW)
+
+#define GET_BIT(DATA, N)        ((DATA>>N) & 0x0001)
+
+void ad8400_write(uint8_t addr, uint8_t value)
+{
+	uint16_t send_value = (addr<<8) + value;
+	MCP_CS_CLC();
+	for(int i=0; i<10; i++) {
+		MCP_SCK_CLC();
+		if(GET_BIT(send_value, 9-i)) {
+			MCP_SI_SET();
+		} else {
+			MCP_SI_CLC();
+		}
+		MCP_SCK_SET();
+	}
+	MCP_CS_SET();
+}
+
+
+static void ad8400_set_value(int argc, char**argv)
+{
+	if (argc < 2) {
+		rt_kprintf("Please input parm!\n");
+		return;
+	}
+	int value = atoi(argv[1]);
+	if (value > 255) {
+		rt_kprintf("Please input value range : 0~ 255!\n");
+		return;
+	}
+
+	ad8400_write(value);
+}
+/* 导出到 msh 命令列表中 */
+MSH_CMD_EXPORT(ad8400_set_value, ad8400 set value);
+
+int rt_hw_ad8400_init(void)
+{
+	rt_pin_mode(AD8400_CS_PIN, PIN_MODE_OUTPUT);
+	rt_pin_mode(AD8400_SCK_PIN, PIN_MODE_OUTPUT);
+	rt_pin_mode(AD8400_SI_PIN, PIN_MODE_OUTPUT);
+
+	rt_pin_write(AD8400_CS_PIN, PIN_LOW);
+	rt_pin_write(AD8400_SCK_PIN, PIN_LOW);
+	rt_pin_write(AD8400_SI_PIN, PIN_LOW);
+    return 0;
+}
+INIT_DEVICE_EXPORT(rt_hw_ad8400_init);
+#endif
+
+
